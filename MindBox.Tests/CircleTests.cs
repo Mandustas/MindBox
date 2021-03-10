98570252e2a8c2cf781d57bbe94ea9fa401ad8d6
Andrey Podolyako <andreypodolyako@gmail.com>
2021-03-10 17:25:13 +0300

    Заготовлены Unit-тесты

---
+++ MindBox.Tests/CircleTests.cs
@@ -0,0 +1,53 @@
+﻿using FigureSquares;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MindBox.Tests
+{
+    public class CircleTests
+    {
+
+        [Fact]
+        public void SquareTest()
+        {
+            Circle circle = new Circle { R = 4 };
+
+            var circleSquare = circle.Square();
+
+            //Assert
+            Assert.Equal(50.265, Math.Round(circleSquare, 3));
+        }
+        [Fact]
+        public void SquareZeroRTest()
+        {
+            Circle circle = new Circle { R = 0 };
+
+            var circleSquare = circle.Square();
+
+            //Assert
+            Assert.Equal(0, circleSquare);
+        }
+        [Fact]
+        public void SquareNotIntegerRTest()
+        {
+            Circle circle = new Circle { R = 1.5 };
+
+            var circleSquare = circle.Square();
+
+            //Assert
+            Assert.Equal(7.069, Math.Round(circleSquare, 3));
+        }
+        [Fact]
+        public void SquareNegativeRTest()
+        {
+            Circle circle = new Circle { R = -3 };
+
+            //var circleSquare = circle.Square();
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => circle.Square());
+        }
+    }
+}
---
+++ MindBox.Tests/TriangleTests.cs
@@ -0,0 +1,77 @@
+﻿using FigureSquares;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace MindBox.Tests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void TriangleTest()
+        {
+            Triangle triangle = new Triangle { A = 8, B = 6, C = 4 };
+
+            var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Equal(11.619, Math.Round(triangleSquare, 3));
+        }
+        [Fact]
+        public void TriangleNotIntegerSideTest()
+        {
+            Triangle triangle = new Triangle { A = 8.3, B = 6.1, C = 4 };
+
+            var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Equal(11.553, Math.Round(triangleSquare,3));
+        }
+
+        [Fact]
+        public void TriangleZeroSideTest()
+        {
+            Triangle triangle = new Triangle { A = 8, B = 6, C = 0 };
+
+            //var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => triangle.Square());
+        }
+
+        [Fact]
+        public void TriangleNegativeSideTest()
+        {
+            Triangle triangle = new Triangle { A = 8, B = 6, C = -4 };
+
+            //var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => triangle.Square());
+        }
+
+        [Fact]
+        public void RightTriangleTest()
+        {
+            Triangle triangle = new Triangle { A = 3, B = 4, C = 5 };
+
+            var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Equal(6, triangleSquare);
+        }
+
+        [Fact]
+        public void TriangleExistExeptionTest()
+        {
+            Triangle triangle = new Triangle { A = 1, B = 2, C = 3 };
+
+            //var triangleSquare = triangle.Square();
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => triangle.Square());
+        }
+
+
+    }
+}
